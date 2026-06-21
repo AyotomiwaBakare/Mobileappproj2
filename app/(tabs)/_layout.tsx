515c64a660fdde63900e01ec4ad7c326e1ee3cde
AyotomiwaBakare <ayotomiwa.bakare@edu.sait.ca>
2026-06-20 23:41:35 -0600

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,48 +1,9 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
 
-export default function TabLayout() {
+export default function Home() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: "#000",
-          borderTopColor: "#222",
-        },
-        tabBarActiveTintColor: "#fff",
-        tabBarInactiveTintColor: "#888",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Search",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search" size={size} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <View>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
